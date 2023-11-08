15941f5f3b303b364cedf9fe4ea008607d807716
xq443 <18646040956@163.com>
2023-11-08 01:03:13 -0800

    practice

---
+++ DP/Interval/longestPalindromicSubsequence.java
@@ -0,0 +1,39 @@
+package Interval;
+
+public class longestPalindromicSubsequence {
+    /**
+     * A subsequence is a sequence that can be derived from another sequence
+     * by deleting some or no elements without changing the order of the remaining elements.
+     *
+     * Input: s = "bbbab"
+     * Output: 4
+     */
+    public static int longestPalindromeSubseq(String s){
+        int n = s.length();
+        s = "#" + s;
+        int [][] dp = new int[n+1][n+1];
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j <= n ; j++) {
+                dp[i][j] = 0;
+            }
+        }
+        for (int len = 1; len <= n; len++) {
+            for (int i = 1; i + len - 1 <= n ; i++) {
+                int j= i +len - 1;
+                if(len == 1) dp[i][j] = 1;
+                else if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i+1][j-1] +2;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i+1][j]);
+                }
+            }
+        }
+        return dp[1][n];
+
+    }
+
+    public static void main(String[] args) {
+        String s = "bbbab";
+        System.out.println(longestPalindromeSubseq(s));
+    }
+}
--- TwoPointer/validPalindrome.java
+++ TwoPointer/validPalindrome.java
@@ -1,5 +1,3 @@
-package src;
-
 public class validPalindrome {
     public static boolean validPalindrome(String s){
         //base case
