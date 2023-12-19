3ee5c8d5db89f8341e2aacb1d9c4d9649ba86b59
xq443 <18646040956@163.com>
2023-12-18 18:57:43 -0800

    update bfs

---
+++ DFS/removeInvalidParentheses.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class removeInvalidParentheses {
+    int maxSize = -1;
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> res = new ArrayList<>();
+        Set<String> filteredRes = new HashSet<>();
+        dfs(filteredRes, s, 0, new StringBuilder(), 0, 0);
+
+        res.addAll(filteredRes);
+        return res;
+    }
+
+    public void dfs(Set<String> filteredRes, String s, int cur_idx, StringBuilder cur_res, int l_count, int r_count) {
+        if (cur_idx == s.length()) {
+            if (l_count == r_count) {
+                int curLength = cur_res.length();
+                if (curLength > maxSize) {
+                    maxSize = curLength;
+                    filteredRes.clear(); // Clear previous results of smaller lengths
+                    filteredRes.add(cur_res.toString());
+                }
+                if (curLength == maxSize) {
+                    filteredRes.add(cur_res.toString());
+                }
+            }
+            return;
+        }
+
+        char cur_char = s.charAt(cur_idx);
+        if (cur_char == '(') {
+            cur_res.append(cur_char);
+            dfs(filteredRes, s, cur_idx + 1, cur_res, l_count + 1, r_count);
+            cur_res.deleteCharAt(cur_res.length() - 1);
+            dfs(filteredRes, s, cur_idx + 1, cur_res, l_count, r_count);
+        } else if (cur_char == ')') {
+            dfs(filteredRes, s, cur_idx + 1, cur_res, l_count, r_count);
+            if (l_count > r_count) {
+                cur_res.append(cur_char);
+                dfs(filteredRes, s, cur_idx + 1, cur_res, l_count, r_count + 1);
+                cur_res.deleteCharAt(cur_res.length() - 1);
+            }
+        } else {
+            cur_res.append(cur_char);
+            dfs(filteredRes, s, cur_idx + 1, cur_res, l_count, r_count);
+            cur_res.deleteCharAt(cur_res.length() - 1);
+        }
+    }
+    public static void main(String[] args) {
+        removeInvalidParentheses r = new removeInvalidParentheses();
+        String s = "()())()";
+        List<String> ret = r.removeInvalidParentheses(s);
+        System.out.println(ret.toString());
+    }
+}
+
+
