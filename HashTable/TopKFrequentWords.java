bf7363a5b710d08e17198cc1e8219acbf37d033c
Cathy Qin <qin.xuj@northeastern.edu>
2024-04-07 01:20:01 -0700

    refactor

---
+++ HashTable/TopKFrequentWords.java
@@ -0,0 +1,23 @@
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class TopKFrequentWords {
+//
+//  /**
+//   * Given an array of strings words and an integer k,
+//   * Return the answer sorted by the frequency from highest to lowest.
+//   * Sort the words with the same frequency by their lexicographical order.
+//   * @param words
+//   * @param k
+//   * @return
+//   */
+//  public List<String> topKFrequent(String[] words, int k) {
+//    List<String> ret = new ArrayList<>();
+//    if(words == null || words.length == 0 || k == 0) return ret;
+//
+//
+//
+//
+//  }
+//
+//}
--- Stack/MonotonicStack/NumberofVisiblePeopleinaQueue.java
+++ Stack/MonotonicStack/NumberofVisiblePeopleinaQueue.java
@@ -2,30 +2,40 @@ package MonotonicStack;
 
 import java.util.Stack;
 
-public class NumberofVisiblePeopleinaQueue {
-  public int[] canSeePersonsCount(int[] A) {
-    int[] ret = new int[A.length];
-    Stack<Integer> stack = new Stack<>();
-    for (int i = 0; i < A.length; i++) {
-      // if the last element in stack <= A[i] which means only one people stack.pop() can see
-      while(!stack.isEmpty() && A[stack.peek()] <= A[i]) {
-        ret[stack.pop()]++;
-      }
-      // increment the ret[stack.peek()] by 1 when we figure out it is greater than one element
-      if(!stack.isEmpty()) {
-        ret[stack.peek()]++;
-      }
-      stack.push(i);
-    }
-    return ret;
-  }
+//public class NumberofVisiblePeopleinaQueue {
+//  public int[] canSeePersonsCount(int[] A) {
+//    int[] ret = new int[A.length];
+//    Stack<Integer> stack = new Stack<>();
+//    for (int i = 0; i < A.length; i++) {
+//      // if the last element in stack <= A[i] which means only one people stack.pop() can see
+//      while(!stack.isEmpty() && A[stack.peek()] <= A[i]) {
+//        ret[stack.pop()]++;
+//      }
+//      // increment the ret[stack.peek()] by 1 when we figure out it is greater than one element
+//      if(!stack.isEmpty()) {
+//        ret[stack.peek()]++;
+//      }
+//      stack.push(i);
+//    }
+//    return ret;
+//  }
+//  public int[] canSeePersonsCount(int[] A) {
+//    int[] ret = new int[A.length];
+//    int[] count = new int[A.length];
+//    for (int i = 0; i < A.length; i++) {
+//      while(!count.is)
+//
+//    }
+//
+//  }
 
-  public static void main(String[] args) {
-    NumberofVisiblePeopleinaQueue numberofVisiblePeopleinaQueue = new NumberofVisiblePeopleinaQueue();
-    int[] heights = {10,6,8,5,11,9};
-    int[] ret = numberofVisiblePeopleinaQueue.canSeePersonsCount(heights);
-    for (int i = 0; i < ret.length; i++) {
-      System.out.println(ret[i]);
-    }
-  }
-}
+
+//  public static void main(String[] args) {
+//    NumberofVisiblePeopleinaQueue numberofVisiblePeopleinaQueue = new NumberofVisiblePeopleinaQueue();
+//    int[] heights = {10,6,8,5,11,9};
+//    int[] ret = numberofVisiblePeopleinaQueue.canSeePersonsCount(heights);
+//    for (int i = 0; i < ret.length; i++) {
+//      System.out.println(ret[i]);
+//    }
+//  }
+//}
