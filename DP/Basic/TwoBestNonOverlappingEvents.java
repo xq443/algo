b012f9c5c2ce67410a71f965807eb63783da3a4f
Cathy Qin <qin.xuj@northeastern.edu>
2024-04-16 21:39:14 -0700

    dp revise priority queue

--- DP/Basic/TwoBestNonOverlappingEvents.java
+++ DP/Basic/TwoBestNonOverlappingEvents.java
@@ -2,7 +2,7 @@ package Basic;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.TreeMap;
+import java.util.PriorityQueue;
 
 public class TwoBestNonOverlappingEvents {
 
@@ -19,32 +19,58 @@ public class TwoBestNonOverlappingEvents {
    * @return
    */
   public int maxTwoEvents(int[][] events) {
-    // sort the 2d array by the starting time
+    // Arrays.sort(events, (a, b) -> (a[0] == b[0]) ? a[1] - b[1] : a[0] - b[0]);
     Arrays.sort(events, Comparator.comparingInt(a -> a[0]));
-    // treemap : key-> starting time, value-> maxvalue to retrieve the min key each time
-    TreeMap<Integer, Integer> map = new TreeMap<>();
-    map.put(0,0);
+    PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) ->(a[0] - b[0]));
 
-    int prev = 0, ret = 0;
-
+    int maxValue = 0, ret = 0;
     for(int[] event : events) {
+      // get ride details
       int start = event[0];
       int end = event[1];
       int value = event[2];
-
-      // update the prev maxvalue (event start time)
-      while(!map.isEmpty() && map.firstKey() < start) { // non inclusive
-        prev = Math.max(prev, map.remove(map.firstKey()));
+      while(!queue.isEmpty()) {
+        if(start <= queue.peek()[0]) {
+          break; // inclusively overlap
+        }
+        int[] curr = queue.poll();
+        // update the curr maxvalue
+        maxValue = Math.max(maxValue, curr[1]);
       }
-
-      // update the curr time's maxvalue
-      ret = Math.max(ret, prev + value);
-
-      // update the maxvalue record for the event end time in the map
-      map.put(end, Math.max(map.getOrDefault(end, 0), value));
+      // add new element into the min queue, with end time in asc order
+      queue.offer(new int[]{end, value});
+      // update the accumulated value
+      ret = Math.max(ret, maxValue + value);
     }
     return ret;
   }
+//  public int maxTwoEvents(int[][] events) {
+//    // sort the 2d array by the starting time
+//    Arrays.sort(events, Comparator.comparingInt(a -> a[0]));
+//    // treemap : key-> starting time, value-> maxvalue to retrieve the min key each time
+//    TreeMap<Integer, Integer> map = new TreeMap<>();
+//    map.put(0,0);
+//
+//    int prev = 0, ret = 0;
+//
+//    for(int[] event : events) {
+//      int start = event[0];
+//      int end = event[1];
+//      int value = event[2];
+//
+//      // update the prev maxvalue (event start time)
+//      while(!map.isEmpty() && map.firstKey() < start) { // non inclusive
+//        prev = Math.max(prev, map.remove(map.firstKey()));
+//      }
+//
+//      // update the curr time's maxvalue
+//      ret = Math.max(ret, prev + value);
+//
+//      // update the maxvalue record for the event end time in the map
+//      map.put(end, Math.max(map.getOrDefault(end, 0), value));
+//    }
+//    return ret;
+//  }
 
   public static void main(String[] args) {
     TwoBestNonOverlappingEvents twoBestNonOverlappingEvents = new TwoBestNonOverlappingEvents();
