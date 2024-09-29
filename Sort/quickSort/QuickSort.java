a21377afc1336b0ed5a7224bfd2529152263cfdb
Cathy Qin <qin.xuj@northeastern.edu>
2024-09-29 00:45:48 -0700

    change pivot

--- Sort/quickSort/QuickSort.java
+++ Sort/quickSort/QuickSort.java
@@ -8,30 +8,44 @@ public class QuickSort {
    * @param arr int[]
    * @return int[]
    */
-  public void quickSort(int[] arr) {
-    if (arr == null || arr.length == 0) return;
-    quickSort(arr, 0, arr.length - 1);
-  }
+//  public void quickSort(int[] arr) {
+//    if (arr == null || arr.length == 0) return;
+//    quickSort(arr, 0, arr.length - 1);
+//  }
+//
+//  private void quickSort(int[] arr, int low, int high) {
+//    if (low < high) {
+//      int pi = partition(arr, low, high);
+//      quickSort(arr, low, pi - 1);
+//      quickSort(arr, pi + 1, high);
+//    }
+//  }
 
-  private void quickSort(int[] arr, int low, int high) {
-    if (low < high) {
-      int pi = partition(arr, low, high);
-      quickSort(arr, low, pi - 1);
-      quickSort(arr, pi + 1, high);
-    }
-  }
+//  private int[] partition(int[] arr, int low, int high) {
+//    int pivot = arr[low];
+//    int i = high;
+//    for (int j = low; j < high; j++) {
+//      if (arr[j] < pivot) {
+//        i--;
+//        swap(arr, i, j);
+//      }
+//    }
+//    swap(arr, i + 1, high);
+//    return arr;
+//  }
 
-  private int partition(int[] arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-      if (arr[j] < pivot) {
-        i++;
-        swap(arr, i, j);
+  private int[] partition (int arr[], int low, int high) {
+    // first element as pivot
+    int pivot = arr[low];
+    int k = high;
+    for (int i = high; i > low; i--) {
+      if (arr[i] > pivot){
+        swap (arr, i, k);
+        k--;
       }
     }
-    swap(arr, i + 1, high);
-    return i + 1;
+    swap(arr, k, low);
+    return arr;
   }
 
   private void swap(int[] arr, int i, int j) {
@@ -42,8 +56,8 @@ public class QuickSort {
 
   public static void main(String[] args) {
     QuickSort quickSort = new QuickSort();
-    int[] arr = {9, -3, 5, 2, 7, -1, 10, 4};
-    quickSort.quickSort(arr);
+    int[] arr = {822, 35, 294, 322, 175, 67, 517, 987, 154, 473};
+    quickSort.partition(arr, 0, arr.length - 1);
     for (int num : arr) {
       System.out.print(num + " ");
     }
