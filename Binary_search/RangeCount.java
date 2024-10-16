18a199e07d60cd3f21b2abae1c475f3a6d744508
Cathy Qin <qin.xuj@northeastern.edu>
2024-10-15 22:54:39 -0700

    binary search practice

--- Binary_search/RangeCount.java
+++ Binary_search/RangeCount.java
@@ -14,11 +14,14 @@ public class RangeCount {
 
   // (>= a) - (>= b)
   private int binarySearch(int[] nums, int target) {
-    int left = 0, right = nums.length;
-    while (left < right) {
+    int left = 0, right = nums.length - 1;
+    while(left <= right) {
       int mid = left + (right - left) / 2;
-      if (nums[mid] >= target) right = mid;
-      else left = mid + 1;
+      if(nums[mid] >= target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
     }
     return left;
   }
