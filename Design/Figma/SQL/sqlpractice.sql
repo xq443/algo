ef75161a7b8323de20ae2a46d05648eb1b1e2111
Cathy Qin <qin.xuj@northeastern.edu>
2024-11-14 13:22:56 -0800

    translator

--- Design/Figma/SQL/sqlpractice.sql
+++ Design/Figma/SQL/sqlpractice.sql
@@ -66,3 +66,15 @@ SELECT
           END AS salary_class,
       RANK() OVER (PARTITION BY e.department_id ORDER BY e.salary DESC) AS salary_rank
   FROM employees e;
+
+# ddl
+CREATE TABLE Employees (
+     EmployeeID INT PRIMARY KEY,
+     FirstName VARCHAR(50),
+     LastName VARCHAR(50),
+     BirthDate DATE,
+     HireDate DATE,
+     Salary DECIMAL(10, 2),
+     DepartmentID INT,
+     FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
+  );
